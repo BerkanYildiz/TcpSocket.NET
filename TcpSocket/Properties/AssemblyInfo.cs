1ed4b8d2f08e54e07c799d434577bdd88c3c90f2
BerkanYildiz <BerkanYildiz@users.noreply.github.com>
2020-12-05 22:00:24 +0100

    NuGet

---
+++ TcpSocket/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TcpSocket.NET")]
+[assembly: AssemblyDescription("Asynchronous, full-duplex, and lightweight TCP socket/client for .NET.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Berkan YILDIZ")]
+[assembly: AssemblyProduct("TcpSocket.NET")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
